9a13d22450e19ffa235b8f0e9d0b3c22dbe7194d
STBoyden <stboyden@stboyden.xyz>
2020-05-14 04:40:45 +0100

    Updated make.py | made paths platform independent

--- make.py
+++ make.py
@@ -4,30 +4,28 @@ import sys
 import os
 import shutil
 import subprocess
+from pathlib import Path
 
 dir_path = os.getcwd()
 dir_name = dir_path.split("/")[-1].replace("-", "_")
 resource_directory = "assets"
 distribution_dir = "distrib"
 
-#if os.path.exists(f"{dir_path}/target/debug"):
-#    print("path found\n")
-#    shutil.rmtree(f"{dir_path}/target/debug/{resource_directory}")
-
 commands = ' '.join([x for x in sys.argv[1:]])
 
 result = subprocess.call(["cargo"] + sys.argv[1:])
 
 if result:
-    if os.path.exists(f"{dir_path}/target/debug"):
-        shutil.copytree(f"{dir_path}/{resource_directory}", 
-                        f"{dir_path}/target/debug/{resource_directory}")
+    if os.path.exists(Path(f"{dir_path}/target/debug")):
+        shutil.copytree(Path(f"{dir_path}/{resource_directory}"), \
+                        Path(f"{dir_path}/target/debug/{resource_directory}"))
 
-if not os.path.exists(f"{dir_path}/{distribution_dir}"):
-    os.makedirs(f"{dir_path}/{distribution_dir}")
+if not os.path.exists(Path(f"{dir_path}/{distribution_dir}")):
+    os.makedirs(Path(f"{dir_path}/{distribution_dir}"))
 
 print("")
 print(f"--- [ Copying {'Linux' if os.name == 'posix' else 'Windows'} files ] --- ")
-shutil.copytree(f"{dir_path}/{resource_directory}", f"{dir_path}/{distribution_dir}/{resource_directory}")
-shutil.copyfile(f"{dir_path}/target/debug/{dir_name}{'' if os.name == 'posix' else '.exe'}", \
-                f"{dir_path}/{distribution_dir}/{dir_name}{'' if os.name == 'posix' else '.exe'}")
+shutil.copytree(Path(f"{dir_path}/{resource_directory}"), Path(f"{dir_path}/{distribution_dir}/{resource_directory}"))
+shutil.copyfile(Path(f"{dir_path}/target/debug/{dir_name}{'' if os.name == 'posix' else '.exe'}"), \
+                Path(f"{dir_path}/{distribution_dir}/{dir_name}{'' if os.name == 'posix' else '.exe'}"))
+print("Done")
