6c27b00164c0e61f719a4e32e97c27c9aa593916
STBoyden <stboyden@stboyden.xyz>
2020-05-16 02:03:33 +0100

    Refactoring | Placed source files into relevant directories

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -1,49 +1,56 @@
 use raylib::prelude::*;
 
+use crate::components::game::GameStates;
 use crate::gui::gui_component::GuiComponentBehaviour;
+use crate::gui::gui_button::Button;
+use crate::gui::gui_cursor::Cursor;
 
-pub struct Menu {
-    pub current_state: super::game::GameStates,
+pub struct Menu<'a> {
+    pub current_state: &'a mut GameStates,
+    start_game: bool,
 }
 
-impl Menu {
-    pub fn new() -> Self {
+impl Menu<'_> {
+    pub fn new(initial_state: &mut GameStates) -> Menu {
         Menu {
-            current_state: super::game::GameStates::Menu,
+            current_state: initial_state,
+            start_game: false,
         }
     }
 
     pub fn change_state(&mut self, next_state: &str) {
         match next_state {
             "play" => {
-                self.current_state = super::game::GameStates::Playing;
-            }
-            "pause" => {
-                self.current_state = super::game::GameStates::Paused;
+                *self.current_state = GameStates::Playing;
             }
             "quitting" => {
-                self.current_state = super::game::GameStates::Quitting;
+                *self.current_state = GameStates::Quitting;
             }
             "menu" => {  // default (main menu)
-                self.current_state = super::game::GameStates::Menu;
+                *self.current_state = GameStates::Menu;
             }
             _ => {}
         }
     }
 
-    pub fn draw(&mut self, cursor: &super::gui::gui_cursor::Cursor,
-                draw_handler: &mut RaylibDrawHandle, do_quit: &mut bool) {
-        let mut start_button = super::gui::gui_button::Button::new(
+    pub fn draw(&mut self, cursor: &Cursor, draw_handler: &mut RaylibDrawHandle,
+                do_quit: &mut bool) {
+        match self.start_game {
+            true => self.change_state("play"),
+            _ => {}
+        }
+
+        let mut start_button = Button::new(
             "Start".to_string(),
             Vector2::new(0_f32, (draw_handler.get_screen_height() as f32 / 2_f32) - 50_f32),
             Vector2::new(100_f32, 50_f32),
-            Option::None,
+            Option::Some(&mut self.start_game),
             Color::GRAY,
             Color::DARKGRAY,
             Option::None,
         );
 
-        let mut exit_button = super::gui::gui_button::Button::new(
+        let mut exit_button = Button::new(
             "Quit".to_string(),
             Vector2::new(0_f32, start_button.position.y + 50_f32),
             Vector2::new(100_f32, 50_f32),
--- src/gui/gui_component.rs
+++ src/gui/gui_component.rs
@@ -1,5 +1,7 @@
 use raylib::prelude::*;
 
+use super::gui_cursor::Cursor;
+
 // struct GuiComponent {
 // 	pub position: Vector2,
 // 	pub dimensions: Vector2
@@ -7,7 +9,7 @@ use raylib::prelude::*;
 
 
 pub trait GuiComponentBehaviour {
-    fn draw(&mut self, cursor: &super::gui_cursor::Cursor, draw_handler: &mut RaylibDrawHandle);
+    fn draw(&mut self, cursor: &Cursor, draw_handler: &mut RaylibDrawHandle);
     fn is_hovered(&mut self, mouse_position: &Vector2) -> bool;
-    fn is_clicked(&mut self, cursor: &super::gui_cursor::Cursor) -> bool;
+    fn is_clicked(&mut self, cursor: &Cursor) -> bool;
 }
