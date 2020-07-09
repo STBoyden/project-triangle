55c0718dd4499171e3fd4ff0bea231494ca5a320
STBoyden <stboyden@stboyden.xyz>
2020-07-09 20:09:58 +0100

    Updated make python scripts to include the maps dir

--- make.py
+++ make.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 dir_path = os.getcwd()
 dir_name = dir_path.split("/")[-1].replace("-", "_") if os.name == "posix" else dir_path.split("\\")[-1].replace("-", "_")
-resource_directory = "assets"
+copy_directories = ["assets", "maps"]
 distribution_dir = "distrib"
 
 commands = ' '.join([x for x in sys.argv[1:]])
@@ -17,15 +17,17 @@ result = subprocess.call(["cargo"] + sys.argv[1:])
 
 if result:
     if os.path.exists(Path(f"{dir_path}/target/debug")):
-        shutil.copytree(Path(f"{dir_path}/{resource_directory}"), \
-                        Path(f"{dir_path}/target/debug/{resource_directory}"))
+        for directory in copy_directories:
+                shutil.copytree(Path(f"{dir_path}/{directory}"), \
+                            Path(f"{dir_path}/target/debug/{directory}"))
 
 if not os.path.exists(Path(f"{dir_path}/{distribution_dir}")):
     os.makedirs(Path(f"{dir_path}/{distribution_dir}"))
 
 print("")
 print(f"--- [ Copying {'Linux' if os.name == 'posix' else 'Windows'} files ] --- ")
-shutil.copytree(Path(f"{dir_path}/{resource_directory}"), Path(f"{dir_path}/{distribution_dir}/{resource_directory}"))
+for directory in copy_directories:
+    shutil.copytree(Path(f"{dir_path}/{directory}"), Path(f"{dir_path}/{distribution_dir}/{directory}"))
 shutil.copyfile(Path(f"{dir_path}/target/debug/{dir_name}{'' if os.name == 'posix' else '.exe'}"), \
                 Path(f"{dir_path}/{distribution_dir}/{dir_name}{'' if os.name == 'posix' else '.exe'}"))
 
